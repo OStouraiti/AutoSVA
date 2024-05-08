742f5a4aeb2741269aed9599bdb498c760bbeaa6
Sophia <sz7155@princeton.edu>
2024-05-07 20:13:33 -0400

    removed unnecessary formal files

--- ft_fifo/sva/fifo_bind.svh
+++
@@ -1,6 +0,0 @@
-bind fifo fifo_prop
-	#(
-		.INFLIGHT_IDX (INFLIGHT_IDX),
-		.SIZE (SIZE),
-		.ASSERT_INPUTS (0)
-	) u_fifo_sva(.*);
--- ft_ptw/sva/ptw_bind.svh
+++
@@ -1,4 +0,0 @@
-bind ptw ptw_prop
-	#(
-		.ASSERT_INPUTS (0)
-	) u_ptw_sva(.*);
